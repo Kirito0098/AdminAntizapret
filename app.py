5c3b0f9ebc055ee2f54a6b0b5992bc462ad3e103
Kirito0098 <lexa.sirov@gmail.com>
2025-03-26 17:51:16 +0000

    v0.25

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory, make_response
 import subprocess
 import os
-
+ 
 app = Flask(__name__)
 
 # Путь к директориям с конфигурациями
